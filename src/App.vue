665e9d4931e5af06eccdfd3092cfc34ac64b3be5
sofMD <xsofiamaria.32@gmail.com>
2021-11-25 15:13:01 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
-    
+
+    <Header/>
+
+    <main>
+      
+    </main>
+
   </div>
 </template>
 
 <script>
-
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'App',
   components: {
+    Header,
 
   }
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+
+<header class="w-100 p-2">
+    <img src="../assets/logosp.png" alt="">
+</header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+
+header{
+    background-color: #2e3a46;
+
+    img{
+        width: 50px;
+    }
+}
+
+</style>
